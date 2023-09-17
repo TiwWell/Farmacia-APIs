c4ba9971e6911f2e7067857ad80bb2fbbcb55576
TiwWell <wellissonrodrigues12@gmail.com>
2023-09-17 18:32:21 -0300

    Adcionado novos Farmaceuticos no retorno do endpoint "/lista-farmaceutico-string"

--- src/main/java/br/com/farmacia/farmacia/controller/FarmaciaController.java
+++ src/main/java/br/com/farmacia/farmacia/controller/FarmaciaController.java
@@ -28,8 +28,13 @@ public class FarmaciaController {
         List<Farmaceutico> listaFarmaceutico = new ArrayList<>();
 
         Farmaceutico farmaceutico2 = new Farmaceutico("Pedro", "123.456.789-10", "54321/SP");
+        Farmaceutico farmaceutico3 = new Farmaceutico("Julio" , "321.123.231-21", "23451/SP");
+        Farmaceutico farmaceutico4 = new Farmaceutico("Maria" , "312.213.321-32", "32451/SP");
+
 
         listaFarmaceutico.add(farmaceutico2);
+        listaFarmaceutico.add(farmaceutico3);
+        listaFarmaceutico.add(farmaceutico4);
         System.out.println("Listagem de Farmaceuticos concluida");
         return listaFarmaceutico;
     }
