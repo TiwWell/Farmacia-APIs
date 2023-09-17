8d72d3544e144aa4528a8faf742d592273cef53d
Victor Francelino <jvictorfrance@outlook.com>
2023-09-17 18:15:12 -0300

    CrossSite Permission, SQLServer conection and No/AllArgsConstructors

--- src/main/java/br/com/farmacia/farmacia/FarmaciaApplication.java
+++ src/main/java/br/com/farmacia/farmacia/FarmaciaApplication.java
@@ -1,14 +1,9 @@
 package br.com.farmacia.farmacia;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration .class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class FarmaciaApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/farmacia/farmacia/models/Remedio.java
+++ src/main/java/br/com/farmacia/farmacia/models/Remedio.java
@@ -1,11 +1,16 @@
 package br.com.farmacia.farmacia.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Remedio {
     String nome;
     double preco;
-    int quantidadeEstoque;
+    int quantidade;
+    String img;
 
 }
