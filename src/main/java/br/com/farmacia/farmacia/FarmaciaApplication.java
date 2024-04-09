82cf5f7954feafc07d86097f803e7b51e7d82ca7
José Victor <jvictorfrance@outlook.com>
2024-04-08 21:39:12 -0300

    Adicionando cors para localhost:3000

--- src/main/java/br/com/farmacia/farmacia/FarmaciaApplication.java
+++ src/main/java/br/com/farmacia/farmacia/FarmaciaApplication.java
@@ -2,12 +2,25 @@ package br.com.farmacia.farmacia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class FarmaciaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FarmaciaApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/lista-cliente").allowedOrigins("http://localhost:3000");
+			}
+		};
 	}
 
 }
--- src/main/java/br/com/farmacia/farmacia/controller/ClienteController.java
+++ src/main/java/br/com/farmacia/farmacia/controller/ClienteController.java
@@ -18,7 +18,7 @@ public class ClienteController {
 
     @Autowired
     private ClienteService service;
-    @CrossOrigin(origins = "http://localhost:5173")
+    @CrossOrigin(origins = "http://localhost:3000")
     @ApiOperation(value = "Liste clientes na base de dados", response = ClienteResponse.class)
     @GetMapping(value = "/lista-cliente")
     public List<ClienteDTO> listaCliente() throws Exception {
