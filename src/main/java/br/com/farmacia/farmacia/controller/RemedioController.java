f73c0b06089e31132b9b812c06a9ff32bca88f3d
José Victor <jvictorfrance@outlook.com>
2024-06-01 20:08:48 -0300

    Adicionando log nos métodos de remedio

--- src/main/java/br/com/farmacia/farmacia/controller/RemedioController.java
+++ src/main/java/br/com/farmacia/farmacia/controller/RemedioController.java
@@ -4,12 +4,15 @@ import br.com.farmacia.farmacia.models.ClienteResponse;
 import br.com.farmacia.farmacia.models.RemedioDTO;
 import br.com.farmacia.farmacia.models.RemedioResponse;
 import br.com.farmacia.farmacia.service.RemedioService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
@@ -19,6 +22,8 @@ import java.util.List;
 @Api(description = "Endpoints para listar, adicionar, atualizar e desativar remedios de uma farmácia", tags = {"Remedios"})
 public class RemedioController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(RemedioController.class);
+
     @Autowired
     private RemedioService service;
 
@@ -26,17 +31,25 @@ public class RemedioController {
     @ApiOperation(value = "Liste remedios na base de dados", response = ClienteResponse.class)
     @GetMapping(value = "/listar-remedio")
     public List<RemedioDTO> listaRemedios() throws Exception {
+        LOGGER.info("Listando todos os remédios");
         return service.getRemedio();
     }
     @ApiOperation(value = "Desative remedios na base de dados", response = ClienteResponse.class)
     @GetMapping(value = "/desativar-remedio/{id}")
     public RemedioResponse desativarRemedios(@PathVariable int id) {
+        LOGGER.info("Listando remedios por id: {} ", id);
         return service.desativarRemedio(id);
     }
 
     @ApiOperation(value = "Adicione remedios a base de dados", response = ClienteResponse.class)
     @PostMapping(value = "/adicionar-remedio")
     public RemedioResponse adicionarRemedio(@ApiParam(value = "Conjunto de dados para adicionar remedio no banco de dados") @RequestBody @Validated RemedioDTO remedioDTO) throws Exception {
+        // Converte o objeto remedioDTO para JSON
+        ObjectMapper objectMapper = new ObjectMapper();
+        String remedioDTOJson = objectMapper.writeValueAsString(remedioDTO);
+
+        // Loga o JSON
+        LOGGER.info("Adicionando remédio: {}", remedioDTOJson);
         return service.adicionarRemedio(remedioDTO);
     }
 
